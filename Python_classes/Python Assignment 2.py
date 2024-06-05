76ee567fbe91aaf688c9ea9832d85ab1427a9fa5
Josephine Ijoni <joijoni@yahoo.com>
2024-06-05 13:57:54 +0100

    Add files via upload

---
+++ Python_classes/Python Assignment 2.py
@@ -0,0 +1,23 @@
+rr = ["james", "caleb", "matt", "tobi", "paulina"]
+new_arr = []
+for i in arr:
+    arr1=len(arr[0])
+    new_arr.append(arr1)
+    break
+for i in arr:
+        arr1=len(arr[1])
+        new_arr.append(arr1)
+        break
+for i in arr:
+        arr1=len(arr[2])
+        new_arr.append(arr1)
+        break
+for i in arr:
+        arr1=len(arr[3])
+        new_arr.append(arr1)
+        break
+for i in arr:
+        arr1=len(arr[4])
+        new_arr.append(arr1)
+        break
+print(new_arr)
